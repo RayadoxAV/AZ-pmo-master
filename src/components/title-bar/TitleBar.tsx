06bb49bd8f4b9ff3c290bfb764a0eb942935fa1a
Raymundo Paz <raymundo.pazv@gmail.com>
2025-10-02 23:47:55 -0600

    fix_issue-1: Add window control functionality

--- src/components/title-bar/TitleBar.tsx
+++ src/components/title-bar/TitleBar.tsx
@@ -4,17 +4,37 @@
   September 29th, 2025
 */
 
+import React from 'react';
+
+import { getCurrentWindow } from '@tauri-apps/api/window';
+
+
 import './TitleBar.css';
 
 const TitleBar: React.FC = () => {
+
+  const window = getCurrentWindow();
+
+  function handleMinimize(): void {
+    window.minimize();
+  }
+
+  function handleToggleMaximize(): void {
+    window.toggleMaximize();
+  }
+
+  function handleClose(): void {
+    window.close();
+  }
+
   return (
     <div className="title-bar" data-tauri-drag-region>
       <span className="icon"></span>
       <span className="title">Something something</span>
       <div className="window-controls">
-        <button className="window-control codicon codicon-chrome-minimize"></button>
-        <button className="window-control codicon codicon-chrome-maximize"></button>
-        <button className="window-control codicon codicon-chrome-close"></button>
+        <button className="window-control codicon codicon-chrome-minimize" onClick={handleMinimize}></button>
+        <button className="window-control codicon codicon-chrome-maximize" onClick={handleToggleMaximize}></button>
+        <button data-action="close" className="window-control codicon codicon-chrome-close" onClick={handleClose}></button>
       </div>
     </div>
   );
